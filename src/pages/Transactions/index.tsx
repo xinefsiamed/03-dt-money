27ce366a6492abf045c44536f1640c1d0df08ba2
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2022-11-24 15:41:55 -0300

    Adicionando estilização searchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
@@ -10,6 +11,8 @@ export function Transactions() {
 
 
       <TransactionsContainer>
+        <SearchForm />
+
         <TransactionsTable>
           <tbody>
             <tr>
